e6a97cdb181b72d6c0ea37f5819fd2549e14b006
Alex.F <gfreezy@gmail.com>
2025-01-07 12:52:58 +0800

    Initial Commit

---
+++ seeker/seekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  seekerApp.swift
+//  seeker
+//
+//  Created by feichao on 2025/1/7.
+//
+
+import SwiftUI
+
+@main
+struct seekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ seekerTests/seekerTests.swift
@@ -0,0 +1,17 @@
+//
+//  seekerTests.swift
+//  seekerTests
+//
+//  Created by feichao on 2025/1/7.
+//
+
+import Testing
+@testable import seeker
+
+struct seekerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
